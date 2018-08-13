d942ee7a6eec44133df3d42fb78ec52e24d1679c
Crypto347 <crypto347@gmail.com>
2018-08-13 07:11:36 +0000

    Scetion3

--- src/App.js
+++ src/App.js
@@ -4,12 +4,58 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+  state={
+    persons: [
+      {name:'Max', age: 28},
+      {name:'Manu', age: 29},
+      {name:'St', age: 26},
+    ]
+    
+  }
+
+  switchNameHandler = (newName)=>{
+    this.setState({
+      persons: [
+        {name: newName, age: 30},
+        {name:'Manu', age: 29},
+        {name:'St', age: 27},
+      ]
+    })
+  }
+
+  nameChangeHandler = (event)=>{
+    this.setState({
+      persons: [
+        {name:'Max', age: 28},
+        {name: event.target.value, age: 29},
+        {name:'St', age: 26},
+      ]
+    })
+  }
+
   render() {
+    const style = {
+      background: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
     return (
     <div className = 'App'> 
-      <Person age = '56'/>
-      <Person age = '5'> here </Person>
-      <Person age = '6'/>
+    <button style={style} onClick={this.switchNameHandler.bind(this,"Max")}>Switch Name</button>
+      <Person 
+      name={this.state.persons[0].name} 
+      age={this.state.persons[0].age}
+      click={()=>this.switchNameHandler("MAX")}
+      />
+      <Person 
+      name={this.state.persons[1].name} 
+      age={this.state.persons[1].age}
+      changed={this.nameChangeHandler}> here </Person>
+      <Person 
+      name={this.state.persons[2].name} 
+      age={this.state.persons[2].age}/>
     </div>
   
     );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import './Person.css';
+
 const person = (props) =>{
     return(
-        <div>
-            <p>Person is {props.age} years old</p>
+        <div className="Person">
+            <p onClick={props.click}>Person {props.name} is {props.age} years old</p>
             <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name}/>
         </div>
     );
 }
