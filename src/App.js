c80f55a7ab4f0d2939d306cd6c68a104cee3c9c8
Crypto347 <crypto347@gmail.com>
2018-08-14 09:44:35 +0000

    Section4.2

--- src/App.js
+++ src/App.js
@@ -13,14 +13,21 @@ class App extends Component {
     showPersons: false
   }
 
-  nameChangeHandler = (event)=>{
-    this.setState({
-      persons: [
-        {name:'Max', age: 28},
-        {name: event.target.value, age: 29},
-        {name:'St', age: 26},
-      ]
-    })
+  nameChangeHandler = (event, id)=>{
+    const personIndex = this.state.persons.findIndex((p)=>{
+      return p.id === id});
+    
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    
+    persons[personIndex] = person;
+
+    this.setState({persons: persons})
   }
 
   deleteNameHandler = (personIndex) => {
@@ -53,7 +60,8 @@ class App extends Component {
             click={()=>this.deleteNameHandler(index)}
             name={person.name}
             age={person.age}
-            key={person.id}/>
+            key={person.id}
+            changed={(event)=>this.nameChangeHandler(event, person.id)}/>
         })}
       </div>
       );
