a1fc71a213ac276becee0e84a872f580a5a7613e
Crypto347 <crypto347@gmail.com>
2018-08-17 10:14:49 +0000

    Section7.2(Lifecycle-tree)

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,17 +1,28 @@
-import React from 'react';
-
+import React, {Component} from 'react';
 
 import classes from './Person.css';
 
-const person = (props) =>{
-   
-    return(
-        <div className={classes.Person}>
-            <p onClick={props.click}>Person {props.name} is {props.age} years old</p>
-            <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={props.name}/>
-        </div>
-    );
+class Person extends Component{
+    constructor(props){
+        super(props);
+        console.log('[Person.js] inside constructor',props);
+    }
+    componentWillMount(){
+      console.log('[Person.js] inside componentWillMount()');
+    }
+    componentDidMount(){
+      console.log('[Person.js] inside componentDidMount()');
+    }
+    render(){
+        console.log('[Person.js] inside render()');
+        return(
+            <div className={classes.Person}>
+                <p onClick={this.props.click}>Person {this.props.name} is {this.props.age} years old</p>
+                <p>{this.props.children}</p>
+                <input type="text" onChange={this.props.changed} value={this.props.name}/>
+            </div>
+        );
+    }
 }
 
-export default person;
+export default Person;
--- src/containers/App.js
+++ src/containers/App.js
@@ -5,15 +5,24 @@ import Person from '../components/Persons/Persons'
 import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
-  state={
-    persons: [
-      {id: 'ddasf76', name:'Max', age: 28},
-      {id: 'audsc87', name:'Manu', age: 29},
-      {id: '89sdcll', name:'St', age: 26},
-    ],
-    showPersons: false
+  constructor(props){
+    super(props);
+    console.log('[App.js] inside constructor');
+    this.state={
+      persons: [
+        {id: 'ddasf76', name:'Max', age: 28},
+        {id: 'audsc87', name:'Manu', age: 29},
+        {id: '89sdcll', name:'St', age: 26},
+      ],
+      showPersons: false
   }
-
+}
+  componentWillMount(){
+    console.log('[App.js] inside componentWillMount()', this.state);
+  }
+  componentDidMount(){
+    console.log('[App.js] inside componentDidMount()');
+  }
   nameChangeHandler = (event, id)=>{
     const personIndex = this.state.persons.findIndex( p =>{
       return p.id === id});
@@ -44,7 +53,7 @@ class App extends Component {
 
   render() {
     let persons = null;
-    
+    console.log('[App.js] inside render()');
 
     if(this.state.showPersons){
       persons = <Person 
