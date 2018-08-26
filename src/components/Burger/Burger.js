ff47db89f0f532069d19fbc52149020696b843ef
Crypto347 <crypto347@gmail.com>
2018-08-26 07:43:03 +0000

    Section8.4(OutputtingMultBuildContr)

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,13 +4,19 @@ import classes from './Burger.css';
 import BurgerIngredients from './BurgerIngridients/BurgerIngridients';
 
 const burger = (props) => {
-    const transformedIngridients = Object.keys(props.ingredients)
+    let transformedIngridients = Object.keys(props.ingredients)
     .map( igKey => {
         return [...Array(props.ingredients[igKey])].map(( _,i) => {
             return <BurgerIngredients key={igKey + i} type={igKey}/>
         });
-    });
+    })
+    .reduce((arr,el)=>{
+        return arr.concat(el)
+    },[]);
 
+    if(transformedIngridients.length === 0){
+      transformedIngridients = <p>Please start adding ingredients!</p>  
+    }
     return(
         <div className={classes.Burger}>
             <BurgerIngredients type="bread-top"/>
