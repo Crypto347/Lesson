ea7d20d5a4e6123f8a4b5b2dafa41ffe9df8e9c8
Crypto347 <crypto347@gmail.com>
2018-08-28 12:01:13 +0000

    Section8.11(BackdropOrder)

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
 import classes from './Modal.css';
+import Aux from '../../../hoc/Aux';
+import  Backdrop from '../Backdrop/Backdrop';
 
 const modal = (props) => (
-    <div className={classes.Modal}
-        style={{transform: props.show ? 'translate Y/0':'translate Y/(-100)',
-                opacity: props.show ? '1':'0'}}>
-        {props.children}
-    </div>
+    <Aux>
+        <Backdrop show={props.show} clicked={props.modalClosed}/>
+        <div className={classes.Modal}
+            style={{transform: props.show ? 'translate Y/0':'translate Y/(-100)',
+                    opacity: props.show ? '1':'0'}}>
+            {props.children}
+        </div>
+    </Aux>
 );
 
 export default modal;
