a00d60b1219ad3adae59562b2016901bf5c2929e
Crypto347 <crypto347@gmail.com>
2018-08-29 11:17:42 +0400

    Section8.15(Logo)

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import classes from './Toolbar.css';
+import Logo from '../Logo/Logo';
 
 const toolbar = (props) => (
-    <div className={classes.Toolbar}>
+    <header className={classes.Toolbar}>
         <div>MENU</div>
-        <div>LOGO</div>
+        <Logo/>
         <nav>...</nav>
-    </div>
+    </header>
 );
 
 export default toolbar;
