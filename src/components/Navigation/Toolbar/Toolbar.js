ac8f0e388ee38727bc61c26e1466e89c072e77e8
Crypto347 <crypto347@gmail.com>
2018-08-29 10:15:13 +0400

    Section8.14(Toolbar)

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import classes from './Toolbar.css';
+
+const toolbar = (props) => (
+    <div className={classes.Toolbar}>
+        <div>MENU</div>
+        <div>LOGO</div>
+        <nav>...</nav>
+    </div>
+);
+
+export default toolbar;
