a153426bb0dcb0512ccf921769c7abdb2070a2cd
Crypto347 <crypto347@gmail.com>
2018-09-10 14:38:40 +0400

    Section10.4(RemovingOldInterceptors)

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,12 +4,24 @@ import Layout from '../components/Layout/Layout';
 import BurgerBuilder from './BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
+  state = {
+    show: true
+  }
+
+  componentDidMount(){
+    setInterval(()=>{
+      this.setState({show: false});
+    },5000);
+  }
 
   render() {
     return ( 
       <div >
+        {/* <Layout>
+          <BurgerBuilder/> 
+        </Layout>  */}
         <Layout>
-          <BurgerBuilder/>
+          {this.state.show ? <BurgerBuilder/> : null}
         </Layout>
       </div>
     );
